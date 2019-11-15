4e813aad360151d9de1bb28ebc8b0764aa8b87a3
rog <romain.gautier@nimamoh.net>
2019-11-15 03:25:12 +0100

    unused

--- kio-windows-integration/Exceptions/WindowsIntegrationFocusApiException.cs
+++ kio-windows-integration/Exceptions/WindowsIntegrationFocusApiException.cs
@@ -4,14 +4,6 @@ namespace kio_windows_integration.Exceptions
 {
     public class WindowsIntegrationFocusApiException : Exception
     {
-        public WindowsIntegrationFocusApiException() : base()
-        {
-        }
-
-        public WindowsIntegrationFocusApiException(string msg) : base(msg)
-        {
-        }
-
         public WindowsIntegrationFocusApiException(string msg, Exception inner) : base(msg, inner)
         {
         }
