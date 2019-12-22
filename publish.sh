5cdc5653ab81de38f345fde9fd5301f3578332d9
rog <romain.gautier@nimamoh.net>
2019-12-22 22:14:17 +0100

    Update publish script since the last one contained an error

--- publish.sh
+++ publish.sh
@@ -3,9 +3,9 @@
 cd Kaleidoscope_Companion && dotnet.exe publish -p:PublishSingleFile=true -p:PublishTrimmed=true -r win-x64 -c Release && cd ..
 cd Kaleidoscope_Companion && dotnet.exe publish -p:PublishSingleFile=true -p:PublishTrimmed=true -r win-x86 -c Release && cd ..
 
-rm Kaleidoscope-Companion-winx64.zip
-zip -r -j kaleidoscope-Companion-x64.zip Kaleidoscope_Companion/bin/Release/netcoreapp3.0/win-x64/publish/
+rm Kaleidoscope-Companion-win-x64.zip
+zip -r -j Kaleidoscope-Companion-win-x64.zip Kaleidoscope_Companion/bin/Release/netcoreapp3.0/win-x64/publish/
 
 
-rm Kaleidoscope-Companion-winx86.zip
-zip -r -j kaleidoscope-Companion-x86.zip Kaleidoscope_Companion/bin/Release/netcoreapp3.0/win-x86/publish/
+rm Kaleidoscope-Companion-win-x86.zip
+zip -r -j Kaleidoscope-Companion-win-x86.zip Kaleidoscope_Companion/bin/Release/netcoreapp3.0/win-x86/publish/
